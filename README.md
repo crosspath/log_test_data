a57b1ffbf464d9560b66617090e9b43c4923b644
crosspath <xpath@e1.ru>
2015-09-14 16:04:52 +0300

    sample data

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# log_test_data
+# Log Test Data
 Test data for Redmine Log plugin, based on real data.
+
+## How to install
+Copy to <plugin_dir>/db and run rake db:seed inside <plugin_dir>.
---
+++ seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+
+require 'csv'
+
+puts 'Import data...'
+
+CSV.open('db/seed.csv', 'r', col_sep: ';', headers: true, quote_char: '`').each do |csv|
+    user = User.find_or_create_by!(name: csv['user_id'])
+    log = Log.create!(csv.to_h.merge('user_id' => user.id))
+end
+
+puts 'Set session identifier'
+
+Log.find_each do |log|
+  ago = log.created_at - Log::SESSION_LENGTH.minutes
+  prev_log = Log.where(user_id: log.user_id, created_at: ago..log.created_at).where('id <> ?', log.id).order(created_at: :desc).first
+  log.first_id = prev_log.present? ? prev_log.first_id : log.id
+  log.save!
+end
